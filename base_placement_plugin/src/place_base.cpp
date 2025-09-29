018dbc8471dab319f4ad44fc156e8bc938b6f24d
hanbyeolyoonucla <hanbyeolyoon@DESKTOP-TJ3TJET.localdomain>
2025-09-28 22:10:05 -0700

    fixed octree error

--- base_placement_plugin/src/place_base.cpp
+++ base_placement_plugin/src/place_base.cpp
@@ -435,7 +435,6 @@ bool PlaceBase::findbase(std::vector< geometry_msgs::Pose > grasp_poses)
 
       else
       {
-        ROS_INFO("check2: selected method %d", selected_method_);
         sd.associatePose(baseTrnsCol, grasp_poses, PoseColFilter, res);
         ROS_INFO("Size of baseTrnsCol dataset: %lu", baseTrnsCol.size());
         createSpheres(baseTrnsCol, sphereColor, highScoreSp, false);
